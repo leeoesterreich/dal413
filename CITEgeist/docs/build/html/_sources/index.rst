3d5ff302c2a8f8e7e9a0a5ea604737ace9482a6a
dal413 <dal413@htc-n73.crc.pitt.edu>
2025-09-09 14:11:19 -0400

    feat: Add Sphinx documentation and docstrings

---
+++ CITEgeist/docs/build/html/_sources/index.rst
@@ -0,0 +1,21 @@
+.. CITEgeist documentation master file, created by
+   sphinx-quickstart on Tue Sep 10 14:26:53 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to CITEgeist's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ CITEgeist/docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CITEgeist'
+copyright = '2025, Lee/Oesterreich Lab'
+author = 'Lee/Oesterreich Lab'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+
+html_theme_options = {
+    "repository_url": "https://github.com/leeoesterreich/CITEgeist",
+    "use_repository_button": True,
+}
